5b5a1edcdd98645acc9d2edb88b4578a2ed83f1e
samueltezera28 <99389679+samueltezera28@users.noreply.github.com>
2022-06-06 15:17:40 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     new_string = ''
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
-            pass
+            continue
         else:
             new_string = new_string + my_string[i]
     return new_string
