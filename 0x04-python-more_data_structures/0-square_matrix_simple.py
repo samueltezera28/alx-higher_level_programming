65d75c15b8c196c7dc9b21912ce95dcce1d69982
samueltezera28 <samueltezera28@gmail.com>
2022-06-08 13:21:57 +0300

    requirment fail fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,12 @@
 def square_matrix_simple(matrix=[]):
+
     new_matrix = [[x ** 2 for x in row] for row in matrix]
     return new_matrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(square_matrix_simple(matrix))
