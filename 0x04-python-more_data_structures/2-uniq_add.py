c3ce78166db8db3b336ad21abfcaa374bda24855
samueltezera28 <samueltezera28@gmail.com>
2022-06-08 21:52:55 +0300

    new python

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     set1 = set(my_list)
-    result = 0
+    res = 0
     for i in set1:
-        result = result + i
-    return result
+        res = res + i
+    return res
