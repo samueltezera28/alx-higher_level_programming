2b71c72d15cd88551a41d13dbda83c9e2ea7f72d
samueltezera28 <samueltezera28@gmail.com>
2022-06-21 22:17:03 +0300

    pyt

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -66,3 +66,17 @@ class Square:
             for j in range(0, self.__size):
                 print("#", end="")
             print()
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
