76107b70c0062aa50044fe3d16af8ef1e0350afe
samueltezera28 <99389679+samueltezera28@users.noreply.github.com>
2022-06-23 00:34:35 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -9,9 +9,20 @@ class Square:
     Attributes:
     __size (int): square size private
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
         Args:
-            size: square size
+            size: swqare size
+        Raises:
+            TypeError: if size is not type int
+            ValueError: if size is negative
         """
         self.__size = size
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
