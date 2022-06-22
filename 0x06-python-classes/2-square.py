a265db6646337db181eafd01a3591d3ac4e57259
samueltezera28 <99389679+samueltezera28@users.noreply.github.com>
2022-06-23 00:52:24 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
-    """
+""" Creates an empty class called Square
+"""
 
+
+class Square:
+    """
+    Class definition: Square
+    Attributes:
+    __size (int): square size private
+    """
     def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
