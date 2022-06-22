0bd2c457777d909acf343f378f6ba990ea46d0bb
samueltezera28 <99389679+samueltezera28@users.noreply.github.com>
2022-06-23 00:51:05 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,3 +1,5 @@
+
+
 #!/usr/bin/python3
 """ Creates an empty class called Square
 """
@@ -9,20 +11,9 @@ class Square:
     Attributes:
     __size (int): square size private
     """
-    def __init__(self, size=0):
+    def __init__(self, size):
         """
         Args:
-            size: swqare size
-        Raises:
-            TypeError: if size is not type int
-            ValueError: if size is negative
+            size: swuare size
         """
         self.__size = size
-
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
-    def area(self):
-        return self.__size ** 2
