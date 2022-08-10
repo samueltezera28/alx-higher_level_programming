b8d2a3577f2f4ecb2ee3b5ed4fd7f7a095cdfb4e
samueltezera28 <samueltezera28@gmail.com>
2022-08-10 16:52:51 +0300

    sql_intro

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- deletes a database called hbtn_0c_0
+-- if the database doesnt exits it will not fail
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- it creates a table called first_table 
+-- creates a table if even if the table already exists
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
