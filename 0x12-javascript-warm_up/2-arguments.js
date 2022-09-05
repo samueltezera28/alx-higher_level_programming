16389fe4d2584c0fd01ef1a9c43078a6645d279b
samueltezera28 <samueltezera28@gmail.com>
2022-09-05 23:21:40 +0300

    meow

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (Process.argv.length === 0) {
+    console.log('No argument');
+} else if (Process.argv.length ===1) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
