4cb914f09fdcfac0b081e28f38624cdf1559e1c2
samueltezera28 <samueltezera28@gmail.com>
2022-09-05 23:24:42 +0300

    s

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
-#!/usr/bin/node
+
 if (Process.argv.length === 0) {
     console.log('No argument');
-} else if (Process.argv.length ===1) {
+} else if (Process.argv.length === 1) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
