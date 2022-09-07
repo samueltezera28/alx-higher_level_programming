4a502e2a6946038a308a5f0d64a22235532abdb1
samueltezera28 <samueltezera28@gmail.com>
2022-09-07 21:58:29 +0300

    dummy fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const sami = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < 3; i++) {
-  console.log(sami[i]);
+  console.log(s[i]);
 }
