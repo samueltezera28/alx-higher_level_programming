3db0c0ed4dea2b747e03413f531a60af5012692f
samueltezera28 <samueltezera28@gmail.com>
2022-09-09 22:06:22 +0300

    db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0usa """
+"""lists all states from the database hbtn_0e_0_usa"""
 
+import MySQLdb
+import sys
 if __name__ == '__main__':
-
-    import MySQLdb
-    import sys
 
     db = MySQLdb.connect(host='localhost',
                          port=3306,
@@ -12,8 +11,10 @@ if __name__ == '__main__':
                          passwd=sys.argv[2],
                          db=sys.argv[3])
 
-    cur = db.cursor()
+    cursor = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    rows = cur.fetchall()
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
+    cursor.close()
+    db.close()
