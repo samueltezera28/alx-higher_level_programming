01655805b537c3b40a8f70d561aa7742b9b93082
samueltezera28 <samueltezera28@gmail.com>
2022-10-18 23:44:14 +0300

    forbidden

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf-8', function (err, result) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(result);
-  }
+require('fs').readFile(process.argv[2], 'utf-8', (e, r) => {
+  if (e) console.log(e);
+  else console.log(r);
 });
