8cb78d439b2cb4db91f2ccde5fea3e0a9acc381e
samueltezera28 <samueltezera28@gmail.com>
2022-10-25 18:08:08 +0300

    new

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
 
-const request = require('request');
-const fs = require('fs');
-
-request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  try {
-    fs.writeFile(process.argv[3], body, 'utf8', function (err, result) { if (err) console.log(err); });
-  } catch (err) {
-    console.log(err);
-  }
+require('request')(process.argv[2], (e, r, b) => {
+  require('fs').writeFile(process.argv[3], b, e => { if (e) console.log(e); });
 });
