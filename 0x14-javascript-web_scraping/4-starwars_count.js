c4997504c225264a782ca277432743d953e91d6b
samueltezera28 <samueltezera28@gmail.com>
2022-10-25 18:04:58 +0300

    starwars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,13 +6,6 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   }
-  /* const nb = JSON.parse(body).results.reduce((acc, elem) => {
-    acc += elem.characters.reduce((acc, character) => {
-      return (character === 'https://swapi.co/api/people/18/' ? acc + 1 : acc);
-    }, 0);
-    return (acc);
-  }, 0);
-  */
   const nb = JSON.parse(body).results.filter((elem) => {
     return elem.characters.filter((url) => { return url.includes('18'); }).length;
   }).length;
